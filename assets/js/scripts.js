9635ac97a25d2d68a8e0cbe51dd869b63588d922
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-10-23 17:57:50 -0600

    nueva imagen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
 function abrirPdf() {
-    window.open('https://docs.google.com/viewerng/viewer?url=https://catalogospiacere.netlify.app/assets/pdf/stockpicacho.pdf', '_blank');
+    window.open('https://docs.google.com/viewerng/viewer?url=https://piacerepedregal.netlify.app/assets/pdf/catalogoventadeaniversario.pdf', '_blank');
 }
 
